9940d0cc2042160e5fc120170392405db38570b2
Daksh Khandal <khandaldaksh@gmail.com>
2026-09-09 19:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.44%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     long long countCommas(int n) {
-        long long ans = 0;
-        
-        long long start = 1000;
-        long long commas = 1;
-        
-        while (start <= n) {
-            long long end = min((long long)n, start * 1000 - 1);
-            
-            ans += (end - start + 1) * commas;
-            
-            start *= 1000;
-            commas++;
-        }
-        
-        return ans;
+       return max(0,n-999);
     }
 };
